c7f6a4a12ed5ecfc105b5b273f5d973b07431805
Janice Chan <janice.chan1288@gmail.com>
2022-07-31 12:19:43 -0400

    commit tree 9685a685cfb5008904aeb3a9e77a517d48dd4887

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
+    if params[:name]
+      students = Student.find_by(first_name: params[:name])
+    else    
     students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
